1e9710e21b529f719f73da284a057b2cde8dceaf
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-09-25 09:13:01 -0500

    section 3

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import {ReactQueryDevtools} from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="App">
       <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
+      {/* <InfinitePeople /> */}
+      <InfiniteSpecies />
+      <ReactQueryDevtools />
     </div>
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -8,6 +9,22 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {data, hasNextPage, isLoading, isFetching, fetchNextPage, isError, error} = useInfiniteQuery('sw-species',  
+  ({pageParam = initialUrl}) => fetchUrl(pageParam), {
+     getNextPageParam: (lastPage) => lastPage.next || undefined
+  })
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error! {error.toString()}</div>;
+
+  return <>
+  {isFetching && <div className="loading">Loading...</div>}
+  <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
+    {data.pages.map(pageData => {
+      return pageData.results.map(species => {
+        return (<Species key={species.name} name={species.name} averageLifespan={species.average_lifespan} language={species.language} />);
+      })
+    })}
+  </InfiniteScroll>
+  </>;
 }
