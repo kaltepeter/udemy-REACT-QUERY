b2f00ebce6005ac17775ee4d2a817564f43f49de
Kayla Altepeter <kayla@kaylaaltepeter.com>
2022-09-26 09:16:04 -0500

    section 8

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,37 +1,78 @@
 import jsonpatch from 'fast-json-patch';
+import { UseMutateFunction, useMutation, useQueryClient } from 'react-query';
+import { queryKeys } from 'react-query/constants';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
 import { useUser } from './useUser';
 
 // for when we need a server function
-// async function patchUserOnServer(
-//   newData: User | null,
-//   originalData: User | null,
-//  ): Promise<User | null> {
-//   if (!newData || !originalData) return null;
-//   // create a patch for the difference between newData and originalData
-//   const patch = jsonpatch.compare(originalData, newData);
+async function patchUserOnServer(
+  newData: User | null,
+  originalData: User | null,
+): Promise<User | null> {
+  if (!newData || !originalData) return null;
+  // create a patch for the difference between newData and originalData
+  const patch = jsonpatch.compare(originalData, newData);
 
-//   // send patched data to the server
-//   const { data } = await axiosInstance.patch(
-//     `/user/${originalData.id}`,
-//     { patch },
-//     {
-//       headers: getJWTHeader(originalData),
-//     },
-//   );
-//   return data.user;
-//  }
+  // send patched data to the server
+  const { data } = await axiosInstance.patch(
+    `/user/${originalData.id}`,
+    { patch },
+    {
+      headers: getJWTHeader(originalData),
+    },
+  );
+  return data.user;
+}
 
-// TODO: update type to UseMutateFunction type
-export function usePatchUser(): (newData: User | null) => void {
+export function usePatchUser(): UseMutateFunction<
+  User,
+  unknown,
+  User,
+  unknown
+> {
+  const queryClient = useQueryClient();
   const { user, updateUser } = useUser();
+  const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  const patchUser = (newData: User | null) => {
-    // nothing to see here
-  };
+  const { mutate: patchUser } = useMutation(
+    (newUserData: User) => patchUserOnServer(newUserData, user),
+    {
+      onMutate: async (newData: User | null) => {
+        // cancel outgoing queries
+        queryClient.cancelQueries([queryKeys.user]);
+        // snapshot of the previous value
+        const previousUserData = queryClient.getQueryData<User>(queryKeys.user);
+        // optimistic update the cache
+        updateUser(newData);
+        // return context
+        return { previousUserData };
+      },
+      onError: (err, newData, context) => {
+        if (context) {
+          updateUser(context.previousUserData);
+          toast({
+            title: 'Error updating user',
+            status: 'error',
+          });
+        }
+      },
+      onSettled: () => {
+        // invalidate queries
+        queryClient.invalidateQueries([queryKeys.user]);
+      },
+      onSuccess: (userData) => {
+        if (user) {
+          toast({
+            title: 'User updated!',
+            status: 'success',
+          });
+        }
+      },
+    },
+  );
 
   return patchUser;
 }
